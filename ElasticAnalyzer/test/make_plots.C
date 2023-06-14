702b2a7d5cbf2ccdf7ec9111382aee2f91152419
Frici <fnemes@cern.ch>
2023-06-14 14:59:07 +0200

    ÃPlots

--- ElasticAnalyzer/test/make_plots.C
+++ ElasticAnalyzer/test/make_plots.C
@@ -1,11 +1,36 @@
 
 void make_plots()
 {
-	TFile *myfile = new TFile("../output.root") ;
+	vector<string> filenames ;
+	
+	filenames.push_back("dx_103_104") ;
+	filenames.push_back("dx_103_104") ;
+	filenames.push_back("dx_103_105") ;
+	filenames.push_back("dx_123_124") ;
+	filenames.push_back("dx_123_125") ;
+	filenames.push_back("dx_23_24") ;
+	filenames.push_back("dx_23_25") ;
+	filenames.push_back("dx_3_4") ;
+	filenames.push_back("dx_3_5") ;
+	filenames.push_back("dy_103_104") ;
+	filenames.push_back("dy_103_105") ;
+	filenames.push_back("dy_123_124") ;
+	filenames.push_back("dy_123_125") ;
+	filenames.push_back("dy_23_24") ;
+	filenames.push_back("dy_23_25") ;
+	filenames.push_back("dy_3_4") ;
+	filenames.push_back("dy_3_5") ;
+
+	TFile *myfile = new TFile("output.root") ;
 
 	TCanvas c ;
 	
-	TH1F *hist1 = (TH1F *)myfile->Get("dx_103_104") ;
-	hist1->Draw("colz") ;
+	for(int i = 0 ; i < filenames.size() ; ++i)
+	{
+		TH1F *hist1 = (TH1F *)myfile->Get(filenames[i].c_str()) ;
+		hist1->Draw("colz") ;
+	
+		c.SaveAs(("plots/" + filenames[i] + ".pdf").c_str()) ;
+	}
 
 }
