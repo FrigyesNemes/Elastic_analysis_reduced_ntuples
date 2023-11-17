05f590ebfb0add8e799047bbdb6204cdb778b9ff
Frici <fnemes@cern.ch>
2023-11-17 16:42:44 +0100

    For inefficiency 3/4 and 2/4

---
+++ ElasticAnalyzer/test/final/ntuples/cms_job.sh
@@ -0,0 +1,47 @@
+cmsRun ../config/cfg_analyzer_template_run_324575_0_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324575_1_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324576_0_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324576_1_re_reco_Totem1_LBRT.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324578_0_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_1_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_2_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_3_re_reco_Totem1_LBRT.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324578_4_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_5_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_6_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_7_re_reco_Totem1_LBRT.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324579_0_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324579_1_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324579_2_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324580_0_re_reco_Totem1_LBRT.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324580_1_re_reco_Totem1_LBRT.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324581_0_re_reco_Totem1_LBRT.py &> /dev/null &
+wait
+
+
+cmsRun ../config/cfg_analyzer_template_run_324575_0_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324575_1_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324576_0_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324576_1_re_reco_Totem1_LTRB.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324578_0_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_1_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_2_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_3_re_reco_Totem1_LTRB.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324578_4_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_5_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_6_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324578_7_re_reco_Totem1_LTRB.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324579_0_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324579_1_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324579_2_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324580_0_re_reco_Totem1_LTRB.py &> /dev/null &
+wait
+cmsRun ../config/cfg_analyzer_template_run_324580_1_re_reco_Totem1_LTRB.py &> /dev/null &
+cmsRun ../config/cfg_analyzer_template_run_324581_0_re_reco_Totem1_LTRB.py &> /dev/null &
