63be3e43f386b14a75a16493cbc62393d676ea7b
Frici <fnemes@cern.ch>
2024-03-01 11:13:46 +0100

    Ok

--- ElasticAnalyzer/test/final_high_beta_star/ntuples/src/plot.cc
+++ ElasticAnalyzer/test/final_high_beta_star/ntuples/src/plot.cc
@@ -39,13 +39,18 @@ using namespace std;
 
 
 vector<string> plots_to_save ;
+
 vector<string> plots_to_save2 ;
 vector<string> plots_to_save3 ;
+
+vector<string> plots_to_save4 ;
+vector<string> plots_to_save5 ;
+vector<string> plots_to_save6 ;
 
 
 int main(int argc, char *argv[])
 {
-  if(argc != 3) 
+  if(argc != 4) 
   {
     cout << "Please provide the filename!" << endl ;
     exit(1) ;
@@ -73,6 +78,9 @@ int main(int argc, char *argv[])
     string plot_to_save = plots_to_save[i] ;
   
     TH2F *histo = (TH2F *)file->Get(plot_to_save.c_str()) ;
+    
+    histo->GetXaxis()->SetTitle("x (mm)") ;
+    histo->GetYaxis()->SetTitle("y (mm)") ;
     
     if(histo != NULL)
     {
@@ -107,6 +115,8 @@ int main(int argc, char *argv[])
   
   plots_to_save_file2.close() ;
   gStyle->SetLineScalePS(.3) ;
+  
+  const bool overlap = true ;
 
   for(int i = 0 ; i < plots_to_save2.size() ; ++i)
   {
@@ -116,6 +126,31 @@ int main(int argc, char *argv[])
     TH2F *histo2 = (TH2F *)file->Get(plot_to_save2.c_str()) ;
     TH2F *histo3 = (TH2F *)file->Get(plot_to_save3.c_str()) ;
     
+    histo2->GetXaxis()->SetTitle("x (mm)") ;
+    histo2->GetYaxis()->SetTitle("y (mm)") ;
+    histo3->GetXaxis()->SetTitle("x (mm)") ;
+    histo3->GetYaxis()->SetTitle("y (mm)") ;
+    
+    string chunk = plot_to_save2.substr(0,2) ;
+    
+    cout << "chunk "  << chunk << endl ;
+    
+    if(chunk.compare("dx") == 0) 
+    {
+      histo2->GetXaxis()->SetTitle("x (mm)") ;
+      histo2->GetYaxis()->SetTitle("dx (mm)") ;
+      histo3->GetXaxis()->SetTitle("x (mm)") ;
+      histo3->GetYaxis()->SetTitle("dx (mm)") ;
+    }
+    else if(chunk.compare("dy") == 0) 
+    {
+      histo2->GetXaxis()->SetTitle("y (mm)") ;
+      histo2->GetYaxis()->SetTitle("dy (mm)") ;
+      histo3->GetXaxis()->SetTitle("y (mm)") ;
+      histo3->GetYaxis()->SetTitle("dy (mm)") ;
+    }
+    
+    
     if(histo2 != NULL)
     {
       histo2->SetMarkerColor(kRed) ;
@@ -124,19 +159,23 @@ int main(int argc, char *argv[])
       histo2->SetMarkerStyle(20) ;
       histo3->SetMarkerStyle(20) ;
 
-      histo2->SetMarkerSize(0.4) ;
-      histo3->SetMarkerSize(0.1) ;
+      histo2->SetMarkerSize(0.6) ;
+      histo3->SetMarkerSize(0.4) ;
 
       histo2->Draw("scat") ;
-      histo3->Draw("same scat") ;
+      if(overlap) histo3->Draw("same scat") ;
 
-      c.SetLogz() ;
+      // c.SetLogz() ;
  
       c.SetGridx() ;
       c.SetGridy() ;
+      
+      string postfix = "" ;
+      if(overlap) postfix = "_overlap" ;
 
-      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save2 + ".pdf").c_str()) ;
-      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save2 + ".png").c_str()) ;
+      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save2 + postfix + ".pdf").c_str()) ;
+      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save2 + postfix +  ".root").c_str()) ;
+      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save2 + postfix +  ".png").c_str()) ;
     }
     else
     {
@@ -145,5 +184,88 @@ int main(int argc, char *argv[])
   
   }
 
+
+  const string filename3(argv[3]) ;  
+
+  ifstream plots_to_save_file3(filename3.c_str()) ;
+
+  while(plots_to_save_file3 >> word)
+  {
+    plots_to_save4.push_back(word) ;
+
+    plots_to_save_file3 >> word ;
+
+    plots_to_save5.push_back(word) ;
+
+    plots_to_save_file3 >> word ;
+
+    plots_to_save6.push_back(word) ;
+  }
+  
+  plots_to_save_file3.close() ;
+  gStyle->SetLineScalePS(.3) ;
+
+  for(int i = 0 ; i < plots_to_save4.size() ; ++i)
+  {
+    string plot_to_save4 = plots_to_save4[i] ;
+    string plot_to_save5 = plots_to_save5[i] ;
+    string plot_to_save6 = plots_to_save6[i] ;
+  
+    TH2F *histo4 = (TH2F *)file->Get(plot_to_save4.c_str()) ;
+    TH2F *histo5 = (TH2F *)file->Get(plot_to_save5.c_str()) ;
+    TH2F *histo6 = (TH2F *)file->Get(plot_to_save6.c_str()) ;
+    
+    if(histo4 != NULL)
+    {
+
+      histo5->GetXaxis()->SetTitle("x (mm)") ;
+      histo5->GetYaxis()->SetTitle("#Delta x (mm)") ;
+
+      histo4->SetMarkerColor(kRed) ;
+      histo4->SetLineColor(kRed) ;
+      histo4->SetLineWidth(0.5) ;
+      histo5->SetMarkerColor(kBlue) ;
+      histo6->SetMarkerColor(kGreen) ;
+
+      histo4->SetMarkerStyle(20) ;
+      histo5->SetMarkerStyle(20) ;
+      histo6->SetMarkerStyle(20) ;
+
+      histo4->SetMarkerSize(0.1) ;
+      histo5->SetMarkerSize(0.1) ;
+      histo6->SetMarkerSize(0.1) ;
+
+      // histo5->Draw("same scat") ;
+      histo6->Draw("colz") ;
+      histo4->Draw("same scat") ;
+
+      // c.SetLogz() ;
+ 
+      c.SetGridx() ;
+      c.SetGridy() ;
+
+      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save4 + ".pdf").c_str()) ;
+
+      histo4->Draw("colz") ;
+      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save4 + "_v1.pdf").c_str()) ;
+
+      histo5->Draw("colz") ;
+      histo4->Draw("same scat") ;
+      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save4 + "_v2.pdf").c_str()) ;
+
+      histo5->SetMarkerColor(kRed) ;
+      histo5->SetLineColor(kRed) ;
+      histo5->SetMarkerSize(0.2) ;
+      histo6->Draw("colz") ;
+      histo5->Draw("same colz") ;
+      c.SaveAs(("../fig/2024_02_29_plots_for_meeting/" + plot_to_save4 + "_v3.pdf").c_str()) ;
+
+    }
+    else
+    {
+      cout << "Histogram does not exist in root file" << plot_to_save4 << endl ;
+    }
+  
+  }
   
 }
--- ElasticAnalyzer/test/final_high_beta_star/ntuples/src/run.sh
+++ ElasticAnalyzer/test/final_high_beta_star/ntuples/src/run.sh
@@ -1,1 +1,1 @@
-./plot plots_to_save.txt plots_to_save2.txt
+./plot plots_to_save.txt plots_to_save2.txt plots_to_save3.txt
