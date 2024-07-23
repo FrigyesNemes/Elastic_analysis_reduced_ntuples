c9e1746d42185e5a5a5bb4bb7e93dd3c7f82e7bd
Frici <fnemes@cern.ch>
2024-07-23 15:30:28 +0200

    For zero bias analysis

---
+++ ElasticAnalyzer/test/ZeroBias/HB/proba.sh
@@ -0,0 +1,36 @@
+cmsRun templates/config2/cfg_analyzer_template_run_324457_re_reco_ZeroBias.py  &> tmp/results_324457.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324458_re_reco_ZeroBias.py  &> tmp/results_324458.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324460_re_reco_ZeroBias.py  &> tmp/results_324460.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324461_re_reco_ZeroBias.py  &> tmp/results_324461.txt &
+wait
+cmsRun templates/config2/cfg_analyzer_template_run_324462_re_reco_ZeroBias.py  &> tmp/results_324462.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324464_re_reco_ZeroBias.py  &> tmp/results_324464.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324465_re_reco_ZeroBias.py  &> tmp/results_324465.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324467_re_reco_ZeroBias.py  &> tmp/results_324467.txt &
+wait
+cmsRun templates/config2/cfg_analyzer_template_run_324469_re_reco_ZeroBias.py  &> tmp/results_324469.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324471_re_reco_ZeroBias.py  &> tmp/results_324471.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324485_re_reco_ZeroBias.py  &> tmp/results_324485.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324498_re_reco_ZeroBias.py  &> tmp/results_324498.txt &
+wait
+cmsRun templates/config2/cfg_analyzer_template_run_324509_re_reco_ZeroBias.py  &> tmp/results_324509.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324513_re_reco_ZeroBias.py  &> tmp/results_324513.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324517_re_reco_ZeroBias.py  &> tmp/results_324517.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324518_re_reco_ZeroBias.py  &> tmp/results_324518.txt &
+wait
+cmsRun templates/config2/cfg_analyzer_template_run_324519_re_reco_ZeroBias.py  &> tmp/results_324519.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324520_re_reco_ZeroBias.py  &> tmp/results_324520.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324524_re_reco_ZeroBias.py  &> tmp/results_324524.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324526_re_reco_ZeroBias.py  &> tmp/results_324526.txt &
+wait
+cmsRun templates/config2/cfg_analyzer_template_run_324527_re_reco_ZeroBias.py  &> tmp/results_324527.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324528_re_reco_ZeroBias.py  &> tmp/results_324528.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324529_re_reco_ZeroBias.py  &> tmp/results_324529.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324530_re_reco_ZeroBias.py  &> tmp/results_324530.txt &
+wait
+cmsRun templates/config2/cfg_analyzer_template_run_324531_re_reco_ZeroBias.py  &> tmp/results_324531.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324532_re_reco_ZeroBias.py  &> tmp/results_324532.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324534_re_reco_ZeroBias.py  &> tmp/results_324534.txt &
+cmsRun templates/config2/cfg_analyzer_template_run_324535_re_reco_ZeroBias.py  &> tmp/results_324535.txt &
+wait
+cmsRun templates/config2/cfg_analyzer_template_run_324536_re_reco_ZeroBias.py  &> tmp/results_324536.txt &
--- ElasticAnalyzer/test/ZeroBias/HB/templates/comm2.sh
+++ ElasticAnalyzer/test/ZeroBias/HB/templates/comm2.sh
@@ -1,1 +1,1 @@
-cmsRun templates/config2/cfg_analyzer_template_run_NNNN_re_reco_ZeroBias.py  > tmp/results_NNNN.txt
+cmsRun templates/config2/cfg_analyzer_template_run_NNNN_re_reco_ZeroBias.py  &> tmp/results_NNNN.txt &
